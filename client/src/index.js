f6d7b914e82ca232df841a90b6c6f89ed7dbc59e
Nico Gräf <graef.nico@gmail.com>
2023-11-25 21:59:02 +0100

    chore: remove redux-devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,21 @@
-// React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-// Redux
 import { Provider } from 'react-redux'
-import store from './store'
-
-// Material UI
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme'
 
-// Import App
 import App from './App'
+import theme from './theme'
+import store from './store'
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
   </ThemeProvider>,
 )
-
-/* ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </ThemeProvider>,
-  document.getElementById("root")
-);
- */
