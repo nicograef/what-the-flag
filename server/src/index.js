9fe0b7443775dfa0f7614221a22827d88314f284
Nico Gräf <graef.nico@gmail.com>
2023-11-26 11:59:41 +0100

    rename: NODE_ENV to APP_ENVIRONMENT

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const { Server } = require('./server')
 const SERVER_PORT = process.env.PORT || 5000
 const JWT_SECRET = process.env.JWT_SECRET
 const MONGODB_URI = process.env.MONGODB_URI
-const ENVIRONMENT = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const ENVIRONMENT = process.env.APP_ENVIRONMENT === 'production' ? 'production' : 'development'
 
 async function main() {
   const database = new Database(MONGODB_URI)
