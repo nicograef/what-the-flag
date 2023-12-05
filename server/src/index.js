48827b47c97cf2990d9af8ba9937b16f740d6796
Nico Gräf <graef.nico@gmail.com>
2023-12-05 20:34:11 +0100

    refactor: move service files to services folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,11 @@
 const { Database } = require('./database/database')
-const { JwtService } = require('./jwt-service')
-const { PasswordService } = require('./password-service')
+const { JwtService } = require('./services/jwt-service')
+const { PasswordService } = require('./services/password-service')
 const { Server } = require('./server')
 
 const SERVER_PORT = process.env.PORT || 5000
 const JWT_SECRET = process.env.JWT_SECRET
 const MONGODB_URI = process.env.MONGODB_URI
-// const ENVIRONMENT = process.env.APP_ENVIRONMENT === 'production' ? 'production' : 'development'
 
 async function main() {
   const database = new Database(MONGODB_URI)
